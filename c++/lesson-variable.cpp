8afe2874180cc563b8772ea007c062fe3f5c677f
Ly Ngoc Tram <tramln@bsscommerce.com>
2023-06-12 00:08:46 +0700

    new change

--- c++/lesson-variable.cpp
+++ c++/lesson-variable.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 
+int main(){
+
+    int x = 5;
+    int y = 6;//declaration
+
+    std::cout << x << '\n' ;
+    std::cout << y << '\n' ;
+
+    return 0;
+
+}
---
+++ currency-notes/C++intro.md
@@ -0,0 +1,5 @@
+### C++ intro
+
+* C++ is a low level language
+
+=> High level languages have more computing system underlying it compare to lower level languages.
